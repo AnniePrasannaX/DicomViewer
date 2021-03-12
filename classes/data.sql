72612027884004337c8bf44d15a9ec59ac30ef51
Jisna Siby <jisnasiby09@gmail.com>
2021-03-12 12:39:59 +0530

    updating annie files

---
+++ classes/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO sample.credentials (username,password)
+SELECT * FROM (SELECT 'admin','12345') AS tmp
+WHERE NOT EXISTS (
+        SELECT username FROM sample.credentials WHERE username='admin'
+        )LIMIT 1;
+
