cbf331dceced9327c08686dc585bc61a675948c5
AnniePrasannaX <anniepx97@gmail.com>
2021-03-23 11:31:42 +0530

    Added

---
+++ classes/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS sample.credentials (
+  username VARCHAR(64) PRIMARY KEY,
+  password VARCHAR(64));
+  
+--DROP TABLE IF EXISTS sample.patient_details;
+--CREATE TABLE IF NOT EXISTS sample.patient_details(
+--  patient_id VARCHAR(64) NOT NULL PRIMARY KEY,
+--  patient_name VARCHAR(64),
+--  patient_gender VARCHAR(64),
+--  patient_birthdate VARCHAR(64),
+--  modality VARCHAR(64),
+--  physician_name VARCHAR(64),
+--  study_date VARCHAR(64),
+--  study_time VARCHAR(64),
+--  study_description VARCHAR(64),
+--  study_id VARCHAR(64),
+--  upload_time BIGINT,
+--  dicom_image MEDIUMBLOB);
+
+
